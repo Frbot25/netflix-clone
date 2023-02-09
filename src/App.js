cbed292a407681647f7fe7c027a83852d3af9dff
Frédéric Botella <frederic.botella@gmail.com>
2023-02-09 11:47:55 +0100

    modif featuredMovie.js 5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import MovieSection from './components/MovieSection';
 import FetchApiMovie from './FetchApiMovie';
 import Header from './components/Header';
-import FeaturedMovie from './components/FeaturedMovie';
 import Footer from './components/Footer';
-
+import { FaPlay } from 'react-icons/fa';
+import { RiInformationLine } from 'react-icons/ri';
 function App() {
-  
+  let genres = [];
   window.document.addEventListener('scroll',async (event) => {
     const navbar = document.querySelector('.navbar');
     //console.log('Scrolling...', window.scrollY); 
@@ -43,13 +43,41 @@ function App() {
     }
     loadMoviies();
   }, []);
+  if (featureData) {
+    for (const genre of featureData.genres) {
+      genres.push(genre.name)
+    }
+  }
   return (
     <div className="App">
       <Header />
       <main>
         <>
       {
-        featureData && <FeaturedMovie film={featureData}/>
+        featureData && 
+        <section className='featured' style={{
+          backgroundSize: "cover",
+          backgroundImage: `url(https://image.tmdb.org/t/p/original${featureData.backdrop_path})`,
+          backgroundPosition: 'center'
+        }}>
+          <div className='featured--vertical'>
+            <div className='featured--horizontal'>
+                <div className='featured--name'>{featureData.title}</div>
+                <div className='featured--info'>
+                      <div className='featured--ranking'>{featureData.vote_count}</div>
+                      <div className='featured--year'>{featureData.release_date}</div>
+                </div>
+                <div className='featured--description'>{featureData.overview}</div>
+                <div className='featured--button'>
+                  <a href="/" className='featured--playbutton'><FaPlay /> Lecture</a>
+                  <a href="/" className='featured--mylistbutton'><RiInformationLine /> Plus d'infos</a>
+                  </div>
+                  <div className='featured--genres'>
+                     <strong> Genres : {genres.join(", ")}</strong>
+                  </div>
+            </div>
+          </div>
+        </section>
       }
       </>
       <section className='list'>
