5479470701ce194b3007fbf96a278e36511fea63
Frédéric Botella <frederic.botella@gmail.com>
2023-02-09 11:38:09 +0100

    modif featuredMovie.js 4

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,34 +2,39 @@ import React from 'react'
 import './FeaturedMovie.css'
 import { FaPlay } from 'react-icons/fa';
 import { RiInformationLine } from 'react-icons/ri';
-export default function FeaturedMovie({film}) {
+const FeaturedMovie = ({film})=> {
   let genres = [];
-  for (const genre of film.genres) {
-    genres.push(genre.name)
-  }
-  return (
-    <section className='featured' style={{
-      backgroundSize: "cover",
-      backgroundImage: `url(https://image.tmdb.org/t/p/original${film.backdrop_path})`,
-      backgroundPosition: 'center'
-    }}>
-      <div className='featured--vertical'>
-        <div className='featured--horizontal'>
-            <div className='featured--name'>{film.title}</div>
-            <div className='featured--info'>
-                  <div className='featured--ranking'>{film.title}</div>
-                  <div className='featured--year'>{film.release_date}</div>
-            </div>
-            <div className='featured--description'>{film.overview}</div>
-            <div className='featured--button'>
-              <a href="/" className='featured--playbutton'><FaPlay /> Lecture</a>
-              <a href="/" className='featured--mylistbutton'><RiInformationLine /> Plus d'infos</a>
+  if (film) {
+    for (const genre of film.genres) {
+      genres.push(genre.name)
+    }
+    return (
+      <section className='featured' style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${film.backdrop_path})`,
+        backgroundPosition: 'center'
+      }}>
+        <div className='featured--vertical'>
+          <div className='featured--horizontal'>
+              <div className='featured--name'>{film.title}</div>
+              <div className='featured--info'>
+                    <div className='featured--ranking'>{film.vote_count}</div>
+                    <div className='featured--year'>{film.release_date}</div>
               </div>
-              <div className='featured--genres'>
-                 <strong> Genres : {genres.join(", ")}</strong>
-              </div>
+              <div className='featured--description'>{film.overview}</div>
+              <div className='featured--button'>
+                <a href="/" className='featured--playbutton'><FaPlay /> Lecture</a>
+                <a href="/" className='featured--mylistbutton'><RiInformationLine /> Plus d'infos</a>
+                </div>
+                <div className='featured--genres'>
+                   <strong> Genres : {genres.join(", ")}</strong>
+                </div>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
+  
 }
+
+export default FeaturedMovie;
